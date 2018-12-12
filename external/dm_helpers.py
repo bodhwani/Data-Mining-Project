c8138336225068c111ba30516f4c6ed8e9a516e4
Vinit Bodhwani <vinit.bodhwani2015@vit.ac.in>
2018-12-12 13:00:59 +0530

    Complete project

---
+++ external/dm_helpers.py
@@ -0,0 +1,30 @@
+import nltk
+
+
+
+def rows_format(docs):
+    D = []
+    for d in docs.data:
+        temp_d = " ".join(d.split("\n")).strip('\n\t')
+        D.append([temp_d])
+    return D
+
+def labels_format(target, docs):
+    return docs.target_names[target]
+
+def missing_values_check(row):
+    counter = 0
+    for element in row:
+        if element == True:
+            counter+=1
+    return ("The amoung of missing records is: ", counter)
+
+def tokenize_text(text, remove_stopwords=False):
+    tokens = []
+    for d in nltk.sent_tokenize(text, language='english'):
+        for word in nltk.word_tokenize(d, language='english'):
+            # filters here
+            tokens.append(word)
+    return tokens
+
+
